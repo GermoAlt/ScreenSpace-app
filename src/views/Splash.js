78ae75bb8522ac89d6cdcdad434d761f094a2c02
Germán Altairac <santiago.altairac@kinandcarta.com>
2023-05-07 21:24:10 -0300

    build: react navigation setup

---
+++ src/views/Splash.js
@@ -0,0 +1,9 @@
+import {SafeAreaView, Text} from "react-native";
+
+export default function Splash() {
+    return (
+        <SafeAreaView>
+            <Text style={{color:"black"}}>s</Text>
+        </SafeAreaView>
+    )
+}
