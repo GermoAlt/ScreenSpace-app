66f8fe4a056aebc9614d2c2d5299675d6ec5d021
Germán Altairac <santiago.altairac@kinandcarta.com>
2023-05-22 22:04:04 -0300

    build: ios splash, MUI

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -3,23 +3,37 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Landing from './screens/Landing';
 import SplashScreen from 'react-native-splash-screen';
-const Stack = createNativeStackNavigator();
-function App(): React.JSX.Element {
-  useEffect(() => {
-    SplashScreen.hide(); //hides the splash screen on app load.
-  }, []);
+import {DefaultTheme, PaperProvider} from "react-native-paper";
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={'Landing'}>
-        <Stack.Screen
-          name={'Landing'}
-          component={Landing}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+const Stack = createNativeStackNavigator();
+
+function App(): React.JSX.Element {
+    useEffect(() => {
+        SplashScreen.hide();
+    }, []);
+
+    return (
+        <PaperProvider theme={theme}>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName={'Landing'}>
+                    <Stack.Screen
+                        name={'Landing'}
+                        component={Landing}
+                        options={{headerShown: false}}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </PaperProvider>
+    );
 }
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#282634',
+    secondary: 'yellow',
+  },
+};
 
 export default App;
--- src/ui/screens/Landing.jsx
+++ src/ui/screens/Landing.jsx
@@ -1,10 +1,21 @@
 import {SafeAreaView} from 'react-native';
-import Button from '../components/Button';
+import React from "react";
+import {Button, TextInput} from "react-native-paper";
 
 export default function Landing() {
+
+    const [text, setText] = React.useState("");
   return (
     <SafeAreaView>
-      <Button>boton</Button>
+        <TextInput
+            label="Email"
+            value={text}
+            onChangeText={text => setText(text)}
+            mode={"outlined"}
+        />
+        <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+            Press me
+        </Button>
     </SafeAreaView>
   );
 }
