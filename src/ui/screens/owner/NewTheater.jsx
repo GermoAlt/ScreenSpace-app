ba37b7c85d73b366fac0252eb729d9a547dce0d7
Germán Altairac <santiago.altairac@kinandcarta.com>
2023-06-20 02:33:58 -0300

    feature: new theater layout

--- src/ui/screens/owner/NewTheater.jsx
+++ src/ui/screens/owner/NewTheater.jsx
@@ -1,7 +1,42 @@
-import {SafeAreaView} from "react-native";
+import {SafeAreaView, View} from "react-native";
+import {Dropdown} from "../../components/general/Dropdown";
+import {TextInput} from "../../components/general/TextInput";
+import {useTranslation} from "react-i18next";
+import {Checkbox, Title} from "react-native-paper";
+import {useState} from "react";
+import {Text} from "../../components/general/Text";
+import {SeatLayout} from "../../components/general/SeatLayout";
+import {Button} from "../../components/general/Button";
 
 export const NewTheater = () => {
+    const {t} = useTranslation()
+    const [enabled, setEnabled] = useState(true)
     return (
-        <SafeAreaView></SafeAreaView>
+        <SafeAreaView>
+            <Dropdown/>
+            <TextInput label={t('translation\:owner\.labels\.newTheater\.theaterName')} />
+            <View>
+                <Checkbox status={enabled ? 'checked' : 'unchecked'} onPress={()=>setEnabled(!enabled)}/>
+                <Text>{t("translation\:owner\.labels\.newTheater\.enabled")}</Text>
+            </View>
+
+            <View>
+                <View>
+                    <TextInput />
+                    <Text>{t("translation\:owner\.labels\.newTheater\.row")}</Text>
+                </View>
+                <View>
+                    <TextInput />
+                    <Text>{t("translation\:owner\.labels\.newTheater\.column")}</Text>
+                </View>
+            </View>
+            <View>
+                <Title>{t("translation\:owner\.labels\.newTheater\.screen")}</Title>
+                <SeatLayout />
+            </View>
+            <View>
+                <Button />
+            </View>
+        </SafeAreaView>
     )
 }
