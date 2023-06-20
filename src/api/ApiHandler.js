8abca7ec769c32280e671a66fe606cfe7e57b194
FerFerreyra <ferreyrafernando@gmail.com>
2023-06-19 21:22:49 -0300

    User and Auth Controller

--- src/api/ApiHandler.js
+++ src/api/ApiHandler.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export function getInstance (token) {
     return axios.create({
-        baseURL: 'https://screenspace-back.herokuapp.com/api/v1',
+        baseURL: 'https://screenspace-back.herokuapp.com/v1',
         headers:{
             //'Access-Control-Allow-Origin': '',
             'Content-Type':'application/json',
