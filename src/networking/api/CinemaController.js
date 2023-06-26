e25dc7781fad552508ba5e43be967dd1ebe79226
FerFerreyra <ferreyrafernando@gmail.com>
2023-06-26 01:03:51 -0300

    Create Cinema

--- src/networking/api/CinemaController.js
+++ src/networking/api/CinemaController.js
@@ -1,11 +1,36 @@
 import {getInstance as api} from "./ApiHandler";
+import { getKey } from "../helper";
 
 const prefix = "/cinemas"
 
-export function getCinemasByUser(idUser, token){
-    return api(token).get(prefix)
+export async function getCinemasByUser(idUser, token){
+    return await  api(token).get(prefix)
 }
 
-export function getCinemas(token) {
-    return api(token).get(prefix)
+export async function getCinemas(token) {
+    return await api(token).get(prefix)
+}
+
+export async function postCinemas(body) {
+    const token = await getKey('accessToken')
+    return await api(token).post(prefix, body)
+
+    /*
+{
+    "name": "string",
+    "address": {
+      "street": "string",
+      "number": "string",
+      "neighborhood": "string",
+      "city": "string",
+      "province": "string",
+      "country": "string"
+    },
+    "geoLocation": {
+      "latitude": 0,
+      "longitude": 0
+    },
+    "pricePerFunction": 0
+  }
+  */
 }
--- src/ui/screens/owner/NewCinema.jsx
+++ src/ui/screens/owner/NewCinema.jsx
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import {KeyboardAvoidingView, ScrollView, StyleSheet, View} from "react-native";
 import Swiper from 'react-native-swiper'
 import {TextInput} from "../../components/general/TextInput";
@@ -5,10 +6,109 @@ import {COLORS} from "../../styles/Colors";
 import {useTranslation} from "react-i18next";
 import {Button} from "../../components/general/Button";
 import MapView from "react-native-maps";
+import { ErrorMessage } from '../../components/general/ErrorMessage';
+import { Formik } from 'formik';
+import * as yup from 'yup';
+import { postCinemas } from '../../../networking/api/CinemaController';
+
 export const NewCinema = () => {
     const {t} = useTranslation()
+    const [errMsg, setErrMsg] = React.useState('');
+    const [loading, setLoading] = React.useState(false);
+  
+
+const saveNewCinema = async (values) => {
+
+    setLoading(true)
+    setErrMsg('')
+
+    const body = {
+        name: values.name, 
+        address: {
+            street: values.street,
+            number: values.number,
+            neighborhood: values.neighborhood,
+            city: values.city,
+            province: values.province,
+            country: values.country
+        },
+        geoLocation: {
+            latitude: values.latitude,
+            longitude: values.longitude
+        },
+        pricePerFunction: values.price
+    }
+
+    try {
+
+        console.log('body', body)
+        
+        const response = await postCinemas(body)
+        console.log('response', JSON.stringify(response))
+        if (response.status === 201){
+            navigation.navigate('OwnerNavigator')
+        }else{
+            setErrMsg(t('translation:general.errors.default'));
+        }        
+        
+    } catch (error) {
+        console.log('error', JSON.stringify(error))
+        switch (error.response.data.status){
+            case 400:
+            case 401:
+                setErrMsg(t('translation:login.errors.login.wrongCredentials')); // Bad Request
+            break;
+            case 500: 
+                setErrMsg(t('translation:general.errors.default')); // Internal Server Error
+            break;
+            default:
+                setErrMsg(t('translation:general.errors.default'));
+            break;
+        }
+    }
+    setLoading(false)
+
+}
+
+    const cinemaValidationSchema = yup.object().shape({
+        name: yup
+        .string()
+        .required(t('translation:general.forms.errors.required')),
+        street: yup
+        .string()
+        .required(t('translation:general.forms.errors.required')),
+        number: yup
+        .string()
+        .required(t('translation:general.forms.errors.required')),
+        neighborhood: yup
+        .string()
+        .required(t('translation:general.forms.errors.required')),
+        city: yup
+        .string()
+        .required(t('translation:general.forms.errors.required')),
+        province: yup
+        .string()
+        .required(t('translation:general.forms.errors.required')),
+        country: yup
+        .string()
+        .required(t('translation:general.forms.errors.required')),
+        latitude: yup
+        .string()
+        .required(t('translation:general.forms.errors.required')),
+        longitude: yup
+        .string()
+        .required(t('translation:general.forms.errors.required')),
+    })
+
     return (
         <KeyboardAvoidingView style={{flex:1}}>
+            <Formik
+                initialValues={{ email: '', password: '' }}
+                onSubmit={values => saveNewCinema(values)}
+                validationSchema={cinemaValidationSchema}
+            >
+                {({ handleChange, handleBlur, handleSubmit, values, errors, touched, isValid }) => (
+            <>        
             <Swiper loop={false}
                     dotColor={COLORS.off_white}
                     activeDotColor={COLORS.primary}
@@ -17,39 +117,115 @@ export const NewCinema = () => {
                     showsButtons={true}
             >
 
-                <ScrollView contentContainerStyle={styles.container}>
-                    <TextInput label={t("translation\:owner\.labels\.newCinema\.name")}/>
-                    <TextInput label={t("translation\:owner\.labels\.newCinema\.price")}/>
-                    <TextInput label={t("translation\:owner\.labels\.newCinema\.companyName")}/>
-                    <View style={styles.dualRow}>
-                        <TextInput label={t("translation\:owner\.labels\.newCinema\.street")}
-                                   style={[styles.dualRowElement, styles.big]}/>
-                        <TextInput label={t("translation\:owner\.labels\.newCinema\.number")}
-                                   style={[styles.dualRowElement]}/>
-                    </View>
-                    <TextInput label={t("translation\:owner\.labels\.newCinema\.neighborhood")}/>
-                    <TextInput label={t("translation\:owner\.labels\.newCinema\.locality")}/>
-                    <TextInput label={t("translation\:owner\.labels\.newCinema\.province")}/>
-                    <TextInput label={t("translation\:owner\.labels\.newCinema\.country")}/>
-                </ScrollView>
+                    <ScrollView contentContainerStyle={styles.container}>
+                        {errMsg && (
+                            <ErrorMessage iconType="error">{errMsg}</ErrorMessage>    
+                        )}
+                        <TextInput label={t("translation\:owner\.labels\.newCinema\.name")}
+                        onChangeText={handleChange('name')}
+                        onBlur={handleBlur('name')}
+                        value={values.name}/>
+                        {(errors.name && touched.name) &&
+                            <Text style={styles.errorText}>{errors.name}</Text>
+                        }
+                        
+                        <TextInput label={t("translation\:owner\.labels\.newCinema\.price")}
+                        onChangeText={handleChange('price')}
+                        onBlur={handleBlur('price')}
+                        value={values.price}/>
+                        {(errors.price && touched.price) &&
+                            <Text style={styles.errorText}>{errors.price}</Text>
+                        }
 
-                <View style={styles.container}>
-                    <MapView initialRegion={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
-                    }} style={styles.map}/>
-                    <View style={styles.dualRow}>
-                        <TextInput style={styles.bottomInputs} label={t("translation\:owner\.labels\.newCinema\.latitude")}></TextInput>
-                        <TextInput style={styles.bottomInputs} label={t("translation\:owner\.labels\.newCinema\.longitude")}></TextInput>
-                    </View>
-                </View>
+                        <TextInput label={t("translation\:owner\.labels\.newCinema\.companyName")}
+                        onChangeText={handleChange('companyName')}
+                        onBlur={handleBlur('companyName')}
+                        value={values.companyName}/>
+                        {(errors.companyName && touched.companyName) &&
+                            <Text style={styles.errorText}>{errors.companyName}</Text>
+                        }
+
+                        <View style={styles.dualRow}>
+                            <TextInput label={t("translation\:owner\.labels\.newCinema\.street")}
+                                    style={[styles.dualRowElement, styles.big]}
+                                    onChangeText={handleChange('street')}
+                                        onBlur={handleBlur('street')}
+                                        value={values.street}/>
+                                        {(errors.street && touched.street) &&
+                                            <Text style={styles.errorText}>{errors.street}</Text>
+                                        }
+                            <TextInput label={t("translation\:owner\.labels\.newCinema\.number")}
+                                    style={[styles.dualRowElement]}
+                                    onChangeText={handleChange('number')}
+                                        onBlur={handleBlur('number')}
+                                        value={values.number}/>
+                                        {(errors.number && touched.number) &&
+                                            <Text style={styles.errorText}>{errors.number}</Text>
+                                        }
+                        </View>
+                        <TextInput label={t("translation\:owner\.labels\.newCinema\.neighborhood")}
+                            onChangeText={handleChange('neighborhood')}
+                            onBlur={handleBlur('neighborhood')}
+                            value={values.neighborhood}/>
+                            {(errors.neighborhood && touched.neighborhood) &&
+                                <Text style={styles.errorText}>{errors.neighborhood}</Text>
+                            }
+                        <TextInput label={t("translation\:owner\.labels\.newCinema\.locality")}
+                            onChangeText={handleChange('city')}
+                            onBlur={handleBlur('city')}
+                            value={values.city}/>
+                            {(errors.city && touched.city) &&
+                                <Text style={styles.errorText}>{errors.city}</Text>
+                            }
+                        <TextInput label={t("translation\:owner\.labels\.newCinema\.province")}
+                            onChangeText={handleChange('province')}
+                            onBlur={handleBlur('province')}
+                            value={values.province}/>
+                            {(errors.province && touched.province) &&
+                                <Text style={styles.errorText}>{errors.province}</Text>
+                            }
+                        <TextInput label={t("translation\:owner\.labels\.newCinema\.country")}
+                            onChangeText={handleChange('country')}
+                            onBlur={handleBlur('country')}
+                            value={values.country}/>
+                            {(errors.country && touched.country) &&
+                                <Text style={styles.errorText}>{errors.country}</Text>
+                            }
+                    </ScrollView>
+                    
+
+                    <View style={styles.container}>
+                        <MapView initialRegion={{
+                            latitude: 37.78825,
+                            longitude: -122.4324,
+                            latitudeDelta: 0.0922,
+                            longitudeDelta: 0.0421,
+                        }} style={styles.map}/>
+                        <View style={styles.dualRow}>
+                            <TextInput style={styles.bottomInputs} label={t("translation\:owner\.labels\.newCinema\.latitude")}
+                            onChangeText={handleChange('latitude')}
+                            onBlur={handleBlur('latitude')}
+                            value={values.latitude} />
+                            {(errors.latitude && touched.latitude) &&
+                                <Text style={styles.errorText}>{errors.latitude}</Text>
+                            }
+                            <TextInput style={styles.bottomInputs} label={t("translation\:owner\.labels\.newCinema\.longitude")}
+                            onChangeText={handleChange('longitude')}
+                            onBlur={handleBlur('longitude')}
+                            value={values.longitude}/>
+                            {(errors.longitude && touched.longitude) &&
+                                <Text style={styles.errorText}>{errors.longitude}</Text>
+                            }
+                        </View>
+                    </View>                
             </Swiper>
             <View style={styles.buttonRow}>
-                <Button>asaaas</Button>
+                <Button onPress={handleSubmit}>asaaas</Button>
                 <Button>asaaas</Button>
             </View>
+            </>
+            )}
+            </Formik>
 
         </KeyboardAvoidingView>
     );
