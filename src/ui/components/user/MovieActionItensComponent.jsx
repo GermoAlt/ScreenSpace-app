6b86147a814984bf7df23714bb442518c025f12e
FerFerreyra <ferreyrafernando@gmail.com>
2023-07-09 14:12:46 -0300

    feeat: Share Button

--- src/ui/components/user/MovieActionItensComponent.jsx
+++ src/ui/components/user/MovieActionItensComponent.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import {StyleSheet, View, Image} from "react-native";
-import {COLORS} from "../../styles/Colors";
+import {StyleSheet, View, Share, Pressable} from "react-native";
+
 import {useTranslation} from "react-i18next";
 import { ActionItemComponent } from '../general/ActionItemComponent';
 
@@ -9,12 +9,38 @@ export const MovieActionItensComponent = (props) => {
     const movie  = props.movie
 
     //Para Calificar: star-box-outline ?? comment-edit-outline
+    const onShare = async () => {
+        const bodyMessage = `${t("translation\:user\.labels\.movieSelected\.movie")}: ${movie.title}\n` +  `${t("translation\:user\.labels\.movieSelected\.genre")}: ${movie.genre.join(',')}\n` + `${t("translation\:user\.labels\.movieSelected\.rating")}: ${movie.rating}/5`
+        try {
+          //const result = await Share.share(
+            await Share.share(  
+            { message: bodyMessage,
+              title: 'Mirá la película que podemos ir a ver!' },
+            { dialogTitle: 'ScreenSpace Share'});
+          
+            /* POR AHORA NO NOS IMPORTA EL RESULTADO DEL SHARE
+          if (result.action === Share.sharedAction) {
+            if (result.activityType) {
+              // shared with activity type of result.activityType
+            } else {
+              // shared
+            }
+          } else if (result.action === Share.dismissedAction) {
+            // dismissed
+          }
+          */
+        } catch (error) {
+          alert(error.message);
+        }
+      };
 
     return (
         <View style={styles.container}>
             <ActionItemComponent icon="comment-multiple-outline" label={t("translation\:user\.labels\.movieActionItems\.comments")} />
             <ActionItemComponent icon="comment-edit-outline" label={t("translation\:user\.labels\.movieActionItems\.rate")} />
-            <ActionItemComponent icon="share-variant-outline" label={t("translation\:user\.labels\.movieActionItems\.share")} />
+            <Pressable onPress={() => onShare()}>
+                <ActionItemComponent icon="share-variant-outline" label={t("translation\:user\.labels\.movieActionItems\.share")} />
+            </Pressable>
         </View>
     );
 }
