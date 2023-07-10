38f69cda8da6ca044a54bfc51dd8ae947c656e62
FerFerreyra <ferreyrafernando@gmail.com>
2023-07-10 14:58:58 -0300

    Google SignIn

--- src/ui/screens/Landing.jsx
+++ src/ui/screens/Landing.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable prettier/prettier */
+import * as React from "react";
 import {SafeAreaView} from 'react-native';
 import {Button} from '../components/general/Button';
 import {useTranslation} from 'react-i18next';
@@ -6,7 +7,8 @@ import {useEffect, useState} from 'react';
 import useAuth from '../../hooks/useAuth';
 import useEncryptedStorage from '../../hooks/useEncryptedStorage';
 import {CommonActions, useFocusEffect} from "@react-navigation/native";
-import * as React from "react";
+import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin';
+
 
 export const Landing = ({navigation}) => {
     const {t} = useTranslation();
@@ -17,7 +19,7 @@ export const Landing = ({navigation}) => {
 
     const usarAutenticacion = false  // MANDALE FALSE PARA QUE MUESTRE LA LANDING
     // SI LE MANDASTE FALSE, ESTE CODIGO DE ABAJO TIENE QUE ESTAR PARA QUE USE UN TOKEN
-    const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI2NDkwZWI5MDA2NmM0NDVjNjNkM2Q1M2Usbmljb2xhcy5tYXJ0aW4uY2Fub0BnbWFpbC5jb20sZmFsc2UiLCJJU19PV05FUl9DTEFJTSI6ZmFsc2UsIkVNQUlMX0NMQUlNIjoibmljb2xhcy5tYXJ0aW4uY2Fub0BnbWFpbC5jb20iLCJleHAiOjE2ODkwMTk1OTJ9.H1ovxpLxoSodJkpCDq3NiH7R2yTyrJQg22skPytJjWyoV_O0hHDGD8V0Z231HYB6vOP7MskqvmdwKAniTsrcgA'
+    const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI2NDkwZWI5MDA2NmM0NDVjNjNkM2Q1M2Usbmljb2xhcy5tYXJ0aW4uY2Fub0BnbWFpbC5jb20sZmFsc2UiLCJJU19PV05FUl9DTEFJTSI6ZmFsc2UsIkVNQUlMX0NMQUlNIjoibmljb2xhcy5tYXJ0aW4uY2Fub0BnbWFpbC5jb20iLCJleHAiOjE2ODkwOTcxMTl9.J-e0qko36ZTQnD4xzZJCjKj9yaD0cXOkhPLiR2sMjyS0aIQzzUQN8LuLrs0xeYsHHImdWQqt12G1UYybm3ir1A'
     const setDummyCredentials = async () => {
         const data = { userName: 'nicolas.martin.cano@gmail.com', userPassword: 'prueba', accessToken: token }
         setAuth(data)
@@ -64,6 +66,72 @@ export const Landing = ({navigation}) => {
         })
     }, [navigation])
 
+    const [user, setUser] = React.useState({})
+    
+    React.useEffect(() => {
+        GoogleSignin.configure({
+            webClientId: '133745401400-p070phcl0q8hglb64uakqn82t6i1cog8.apps.googleusercontent.com',
+            //webClientId: '133745401400-enfomql1df2e3qn102k1sgam1ug206rh.apps.googleusercontent.com',
+            offlineAccess: true,
+            forceCodeForRefreshToken: true,
+        })
+        isSignedIn()
+    }, [])
+
+    const signIn = async () => {
+        try {
+            await GoogleSignin.hasPlayServices()
+            const userInfo = await GoogleSignin.signIn()
+            console.log('due___', userInfo)
+            setUser(userInfo)
+        } catch (error) {
+            console.log('Message___', error.message)
+            if (error.code === statusCodes.SIGN_IN_CANCELLED){
+                console.log('User Cancellede the Login Flow')
+            }else if (error.code === statusCodes.IN_PROGRESS){
+                console.log('Signin In')
+            }else if (error.code = statusCodes.PLAY_SERVICES_NOT_AVAILABLE){
+                console.log('Play Services Not Available')
+            }else {
+                console.log('Some other Error Happened')
+            }
+        }
+    }
+
+    const isSignedIn = async () => {
+        const isSignedIn = await GoogleSignin.isSignedIn()
+        if (!!isSignedIn){
+            getCurrentUserInfo()
+        }else{
+            console.log('Plase Login')
+        }
+    }
+
+    const getCurrentUserInfo = async () => {
+        try {
+            const userInfo = await GoogleSignin.signInSilently()
+            console.log('edit___', userInfo)
+            setUser(userInfo)
+        } catch (error) {
+            if (error.code === statusCodes.SIGN_IN_REQUIRED){
+                alert('User has not signed in yet')
+                console.log('User has not signed in yet')
+            }else{
+                alert('Something went wrong')
+                console.log('Something went wrong')
+            }
+        }
+    }
+
+    const signOut = async () => {
+        try {
+            await GoogleSignin.revokeAccess()
+            await GoogleSignin.signOut()
+            setUser({})
+        } catch (error) {
+            console.error(error)
+        }
+    }
 
     return (
         <SafeAreaView>
@@ -91,6 +159,13 @@ export const Landing = ({navigation}) => {
                 onPress={() => navigation.navigate('UserNavigator')}>
                 {t('translation:landing.userNavigateButton')}
             </Button>
+            <GoogleSigninButton
+            size={GoogleSigninButton.Size.Wide}
+            color={GoogleSigninButton.Color.Dark}
+            onPress={signIn}
+            //disabled={this.state.isSigninInProgress}
+            />
+            
         </SafeAreaView>
     );
 }
